9bad43c36400506880b673c8abf22b38ca8fbba0
Filip Fajfer <filipfajfer5@gmail.com>
2023-10-31 12:54:43 +0100

    Feat changed the name of Main class

--- TestDateConverter.java
+++ TestDateConverter.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 
-class Main {
+class TestDateConverter {
     public static void main(String[] args) {
         // IO
         // Date
