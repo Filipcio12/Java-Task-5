afb9b8073f13cb9bffd3f5ab5b45a1a75375cfcf
Filip Fajfer <filipfajfer5@gmail.com>
2023-11-06 17:20:14 +0100

    Unnecessary try catch blocks

--- IO.java
+++ IO.java
@@ -7,35 +7,25 @@ import java.util.List;
 
 public class IO {
     public static String[] readFile(String filePath) throws FileNotFoundException {
-        try {
-            String text = "";
-            File file = new File(filePath);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                text += scanner.nextLine() + "\n";
-            }
-            text = text.substring(0, text.length() - 1);
-            scanner.close();
-            String[] data = text.split("\n");
-            return data;
+        String text = "";
+        File file = new File(filePath);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            text += scanner.nextLine() + "\n";
         }
-        catch (FileNotFoundException ex) {
-            throw ex;
-        }
+        text = text.substring(0, text.length() - 1);
+        scanner.close();
+        String[] data = text.split("\n");
+        return data;
     }
 
     public static void writeFile(String filePath, List<Date> dates) throws IOException {
-        try {
-            File file = new File(filePath);
-            file.createNewFile();
-            FileWriter writer = new FileWriter(filePath);
-            for (Date date : dates) {
-                writer.write(date + "\n");
-            }
-            writer.close();
+        File file = new File(filePath);
+        file.createNewFile();
+        FileWriter writer = new FileWriter(filePath);
+        for (Date date : dates) {
+            writer.write(date + "\n");
         }
-        catch (IOException ex) {
-            throw ex;
-        }
+        writer.close();
     }
 }
